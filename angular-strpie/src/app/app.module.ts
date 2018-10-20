6f0b12d335b3cff8b1e35a15e3910dd5f7bd3ec0
mmorito <morito@mnes.org>
2018-10-21 00:25:21 +0900

    add stripe peyment

--- angular-strpie/src/app/app.module.ts
+++ angular-strpie/src/app/app.module.ts
@@ -1,5 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { StripeComponent } from './components/stripe/stripe.component';
@@ -11,6 +14,8 @@ import { StripeComponent } from './components/stripe/stripe.component';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
