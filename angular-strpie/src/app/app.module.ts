a64d1f53ca0e6338b443da19c8e7d3832523b27a
mmorito <morito@mnes.org>
2018-10-20 22:58:24 +0900

    ng g component components/stripe

--- angular-strpie/src/app/app.module.ts
+++ angular-strpie/src/app/app.module.ts
@@ -2,7 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { ComponentsComponent } from './components/components.component';
 import { StripeComponent } from './components/stripe/stripe.component';
 
 @NgModule({
